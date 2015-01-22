fc594c2a0faf27fb450ca06f0eb6efec4d0e6524
shumpeiKishi <shumpei.kishi@gmail.com>
2015-01-21 19:54:35 -0800

    initial commit

---
+++ template_short_codes.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+Plugin Name: TemplateShortCodes
+Plugin URI: http://shumpeikishi.com/
+Description: This plugin allows users to output template files of your theme from WordPress backend. Create "template" folder in your theme and save template with name like "template-*whatever_you_name*". Thenm, add short code [template_part name="your_template_name"]"].
+Author: Shumpei Kishi 
+Version: 0.0
+Author URI: http://shumpeikishi.com
+License: CC0
+*/
+class AddShortCodes {
+	function __construct () {
+		add_shortcode( 'page_title', array($this, 'page_title'));
+		add_shortcode( 'featured_image_url', array($this, 'featured_image_url'));
+		add_shortcode( 'template_part', array($this, 'template_part'));
+	}
+	function page_title() {
+		return get_the_title();
+	}
+	function featured_image_url() {
+		global $post;
+		if (has_post_thumbnail()) { // check if the post has a Post Thumbnail assigned to it.
+			$thumbnail_id = get_post_thumbnail_id($post->ID);
+			$thumbnail_url = wp_get_attachment_image_src( $thumbnail_id, 'full');
+			return $thumbnail_url[0];
+		} else {
+			return;
+		}
+	}
+
+	function template_part($args) {
+		ob_start();
+		get_template_part("template/template", $args['name']);
+		return ob_get_clean();
+	}
+}
+$addShortCodes = new AddShortCodes();
+?>
